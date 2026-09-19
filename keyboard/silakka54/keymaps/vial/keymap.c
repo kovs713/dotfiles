a5e1166e5e9da2fc35d27fe7bed0fcebfd89bf68
kovs713 <ovsyannikovkostyan@gmail.com>
2026-09-19 11:20:54 +0400

    chore: keyboard

--- keyboard/silakka54/keymaps/vial/keymap.c
+++ keyboard/silakka54/keymaps/vial/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         KC_TAB,   KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                                   KC_Y,        KC_U,         KC_I,         KC_O,         KC_P,            KC_LBRC,
         KC_ESC,   LGUI_T(KC_A), LALT_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G,                                   KC_H,        RCTL_T(KC_J), RSFT_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
-        GRV_CAPS,   KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,                                   KC_N,        KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         BSLS_LANG,
+        KC_GRV,   KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,                                   KC_N,        KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         BSLS_LANG,
 
                                                         NAV,       SYM,       KC_SPC,          KC_ENT,    KC_BSPC,     KC_DEL
     ),
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         KC_TAB,    S(KC_1),     S(KC_2),     S(KC_3),     S(KC_4),     S(KC_5),                                S(KC_6),     S(KC_7),     S(KC_8),     S(KC_9),      S(KC_0),        KC_RBRC,
         KC_ESC,    KC_1,        KC_2,        KC_3,        KC_4,        KC_5,                                   KC_6,        KC_7,        KC_8,        KC_9,         KC_0,           KC_BSLS,
-        S(KC_GRV), S(KC_MINS),  S(KC_EQL),   KC_MINS,     KC_EQL,      KC_NO,                                  KC_NO,       S(KC_LBRC),  S(KC_RBRC),  S(KC_COMM),   S(KC_DOT),      KC_NO,
+        S(KC_GRV), S(KC_MINS),  S(KC_EQL),   KC_MINS,     KC_EQL,      KC_NO,                                  KC_CAPS,       S(KC_LBRC),  S(KC_RBRC),  S(KC_COMM),   S(KC_DOT),      KC_NO,
 
                                                             NAV,       SYM,       KC_SPC,          KC_ENT,    KC_BSPC,     KC_DEL
     ),
